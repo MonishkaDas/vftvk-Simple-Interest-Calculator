4e6e79d59b759770ce6e1e7262a9ebaed421e72b
Monishka Das <monishka29.05@gmail.com>
2021-06-27 17:58:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ function compute()
     var principal = document.getElementById("principal").value;
     if (principal<=0){
         alert("Enter a positive number");
+        return;
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var amount= principal+interest;
     var year = new Date().getFullYear()+parseInt(years);
-    var res="If you deposit \<strong>"+ principal +
-    "\<strong\>,\<br\>at an interest rate of "+ rate +
-    "%\<br\>You will receive an amount of "+ amount +
-    ",\<br\>in the year "+ year +"\<br\>"
+    var res="If you deposit <mark>" + principal +
+    "</mark>,<br/>at an interest rate of <mark>"+ rate +
+    "% </mark> <br/>You will receive an amount of <mark>"+ amount +
+    "</mark>,<br/>in the year <mark>"+ year +"<br/>"
     document.getElementById("result").innerText=res;
 }
 
